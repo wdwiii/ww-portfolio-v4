3985ecc94f88effcefca2f31f5bbc853609c6188
wdwiii <wdwhitfield3@gmail.com>
2023-10-14 11:33:17 -0400

    Add textured background to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from './components/navigation/NavBar'
 import ThemeRegistry from './components/theme/ThemeRegistry'
-import { Container } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +19,16 @@ export default function RootLayout({
   return (
     <ThemeRegistry>
       <html lang='en' style={{ height: '100%' }}>
-        <body className={inter.className} style={{ height: '100%' }}>
-          <Container maxWidth='lg'>{children}</Container>
+        <body
+          className={inter.className}
+          style={{
+            height: '100%',
+            backgroundColor: '#1a1f23',
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h20L0 20z' fill='%23273239' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`,
+          }}
+        >
+          <NavBar />
+          {children}
         </body>
       </html>
     </ThemeRegistry>
