dad0976ec9f7b927e9fbfebb3596919e5f48ef6a
wdwiii <wdwhitfield3@gmail.com>
2023-10-22 02:04:54 -0400

    Remove previous styles, add css baseline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { AppBar, CssBaseline, Toolbar } from '@mui/material'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import HideOnScroll from './components/navigation/HideOnScroll'
 import NavBar from './components/navigation/NavBar'
 import ThemeRegistry from './components/theme/ThemeRegistry'
 
@@ -17,20 +19,28 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ThemeRegistry>
-      <html lang='en' style={{ height: '100%' }}>
-        <body
-          className={inter.className}
-          style={{
-            height: '100%',
-            backgroundColor: '#1a1f23',
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h20L0 20z' fill='%23273239' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`,
-          }}
-        >
-          <NavBar />
-          {children}
-        </body>
-      </html>
-    </ThemeRegistry>
+    <>
+      <CssBaseline />
+      <ThemeRegistry>
+        <html lang='en' style={{ height: '100%' }}>
+          <body
+            className={inter.className}
+            style={{
+              height: '100%',
+            }}
+          >
+            <HideOnScroll>
+              <AppBar color='transparent' elevation={0} sx={{ py: 5 }}>
+                <Toolbar>
+                  <NavBar />
+                </Toolbar>
+              </AppBar>
+            </HideOnScroll>
+            <NavBar sx={{ display: 'none' }} />
+            {children}
+          </body>
+        </html>
+      </ThemeRegistry>
+    </>
   )
 }
