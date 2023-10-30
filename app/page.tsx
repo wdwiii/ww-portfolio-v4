2af8cb5ca8cdccb1ee4ae24c215675efc47cb832
wdwiii <wdwhitfield3@gmail.com>
2023-10-30 00:46:50 -0400

    Install react spotify embed, add to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Stack } from '@mui/material'
+import { Stack, Card, CardContent } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2/Grid2'
 import Hero from './components/homepage/Hero'
 import ImageGallery from './components/homepage/ImageGallery'
 import SubscribeForm from './components/homepage/SubscribeForm'
 import WorkExperienceCard from './components/homepage/WorkExperienceCard'
 import Container from './components/ui/Container'
+import { Spotify } from 'react-spotify-embed'
+import { Typography } from '@mui/material'
+import { Headphones } from '@mui/icons-material'
 
 export default async function Home() {
   return (
@@ -12,8 +15,26 @@ export default async function Home() {
       <Hero />
       <ImageGallery />
       <Container>
-        <Grid container>
-          <Grid xs={12} md={6}></Grid>
+        <Grid container spacing={3}>
+          <Grid xs={12} md={6} py={5}>
+            <Card sx={{ height: '100%' }}>
+              <CardContent sx={{ height: '100%' }}>
+                <Stack height='100%' spacing={1}>
+                  <Stack direction='row' spacing={1} alignItems='center'>
+                    <Headphones />
+                    <Typography variant='h6' component='h2' color='initial'>
+                      What I&apos;m Listening To
+                    </Typography>
+                  </Stack>
+                  <Spotify
+                    style={{ flexGrow: 1 }}
+                    width='100%'
+                    link='https://open.spotify.com/album/79ONNoS4M9tfIA1mYLBYVX'
+                  />
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
           <Grid xs={12} md={6} py={5}>
             <Stack spacing={3}>
               <SubscribeForm />
